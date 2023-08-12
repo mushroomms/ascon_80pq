21e3eecb2f11acc1d0a387bdf0864a5beb4454d0
mushroomms <98047682+mushroomms@users.noreply.github.com>
2023-08-13 01:08:53 +0800

    Add files via upload

--- test_speed/encrypt.c
+++ test_speed/encrypt.c
@@ -1,5 +1,5 @@
-#include "api.h"
-#include "crypto_aead.h"
+#include "lib/api.h"
+#include "lib/crypto_aead.h"
 
 #include <string.h>
 #include <stdio.h>
@@ -7,7 +7,7 @@
 #include <time.h>
 #include <x86intrin.h>
 
-#define CHUNK_SIZE 2048
+#define CHUNK_SIZE 4096
 
 #define cpucycles(cycles) cycles = __rdtsc()
 #define cpucycles_reset() cpucycles_sum = 0
@@ -24,6 +24,8 @@
 unsigned long long cpucycles_before, cpucycles_after, cpucycles_sum;
 
 size_t rlen_total;
+double total_cpucycles;
+struct timespec begin_cpu, end_cpu, begin_wall, end_wall;
 
 static int encrypt(const char *target_file, const char *source_file) {
   #define ADDITIONAL_DATA (const unsigned char *)"123456"
@@ -47,11 +49,23 @@ static int encrypt(const char *target_file, const char *source_file) {
   fwrite(nonce, sizeof(unsigned char), sizeof(nonce), fp_t); // Writing nonce into file
   fread(key, 1, CRYPTO_KEYBYTES, pmk_key); // Reading PMK key file
 
-  while(rlen = fread(buf_in, 1, sizeof buf_in, fp_s)){
+  clock_gettime(CLOCK_REALTIME, &begin_wall);
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin_cpu);
+
+  while(rlen = fread(buf_in, 1, sizeof buf_in, fp_s)) {
+    cpucycles_reset();
+    cpucycles_start();
     crypto_aead_encrypt(buf_out, &clen, buf_in, rlen, ADDITIONAL_DATA, ADDITIONAL_DATA_LEN, (void *)0, nonce, key);
+    cpucycles_stop();
+
     fwrite(buf_out, 1, (size_t)clen, fp_t);
+
+    total_cpucycles += cpucycles_result();
     rlen_total += rlen;
   }
+
+  clock_gettime(CLOCK_REALTIME, &end_wall);
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_cpu);
 
   fclose(fp_t);
   fclose(fp_s);
@@ -60,10 +74,6 @@ static int encrypt(const char *target_file, const char *source_file) {
   }
 
 int main(int argc, char *argv[]) {
-  
-  FILE *cpu_cycle_file = fopen("cpu_cycle_encrypt.txt", "w");
-  struct timespec begin_cpu, end_cpu, begin_wall, end_wall;
-
   if (sodium_init() != 0) {
     printf("panic! the library couldn't be initialized; it is not safe to use");
     return 1;
@@ -71,40 +81,26 @@ int main(int argc, char *argv[]) {
 
   printf("\n[*] Attempting to encrypt key\n");
 
-  clock_gettime(CLOCK_REALTIME, &begin_wall);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin_cpu);
-
-  cpucycles_reset();
-  cpucycles_start();
-
   if (strcmp(argv[1], "secret") == 0){
     if (encrypt("secret.key.hacklab", "secret.key") != 0) {
       return 1;
     }
   }
-
   if (strcmp(argv[1], "pub") == 0){
     if (encrypt("public.key.hacklab", "public.key") != 0) {
       return 1;
     }
   }
-
   if (strcmp(argv[1], "nbit") == 0){
     if (encrypt("nbit.key.hacklab", "nbit.key") != 0) {
       return 1;
     }
   }
 
-  cpucycles_stop();
-
-  clock_gettime(CLOCK_REALTIME, &end_wall);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_cpu);
-
   printf("\n[+] Key encrypted\n");
 
   double total_time_cpu = (end_cpu.tv_sec - begin_cpu.tv_sec) + (end_cpu.tv_nsec - begin_cpu.tv_nsec) / 1000000000.0;
   double total_time_wall = (end_wall.tv_sec - begin_wall.tv_sec) + (end_wall.tv_nsec - begin_wall.tv_nsec) / 1000000000.0;
-  double total_cpucycles = cpucycles_result();
 
   printf("\nWALL time: %f seconds\n", total_time_wall);
   printf("CPU time: %f seconds\n", total_time_cpu);
